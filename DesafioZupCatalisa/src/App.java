7dcef4e9bf4918d55d30a0f79735f7c93aa9dcb3
Luciano Garcia <lualgoritmo@outlook.com>
2022-04-05 18:15:51 -0300

    Faltando terminar

--- DesafioZupCatalisa/src/App.java
+++ DesafioZupCatalisa/src/App.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int opcao = 0;
-        cadastrarUsuario();
-        calcularSalario();
+        //int opcao = 0;
+            cadastrarUsuario();
+            calcularSalario();
+        
         // opcao = sc.nextInt();
         // do {
         // System.out.println("Escolha uma opcao:");
@@ -30,29 +31,36 @@ public class App {
 
     public static void calcularSalario() {
         Scanner leitor = new Scanner(System.in);
-        System.out.println("Salário Mensal");
-        double salario = leitor.nextDouble();
+        double salario = 0;
+        double[] mes = new double[12];
         double imposto = 0;
         double mesPago = 0;
-        
-            if(salario <= 2000.00){
-                System.out.printf("1 Não paga imposto %.2f%n", imposto);
-            }
-            else if(salario >= 2001.00 && salario <= 3000.00){
-                imposto = 0.08;
-                mesPago = salario * imposto;
-                System.out.printf("2 Imposto Pago por mês %.2f%n", mesPago);
-            }
+        for(int i = 0; i < mes.length + 1; i++){
+            System.out.println("Teste 1");
+            mes[i] = leitor.nextDouble();
+        }
+        for(int x= 0; x < mes.length; x++)
+        {
+            System.out.println("Mês: "+ x + mes[x]);
+        }
+            // if(salario <= 2000.00){
+            //     System.out.printf("1 Não paga imposto %.2f%n", imposto);
+            // }
+            // else if(salario >= 2001.00 && salario <= 3000.00){
+            //     imposto = 0.08;
+            //     mesPago = salario * imposto;
+            //     System.out.printf("2 Imposto Pago por mês %.2f%n", mesPago);
+            // }
 
-            else if(salario >= 3001.00 && salario <= 4500.00){
-                imposto = 0.18;
-                mesPago = salario * imposto;
-                System.out.printf("3 Imposto Pago por mês %.2f%n", mesPago);
-            }
-            else {
-                imposto = 0.28;
-                mesPago = salario * imposto;
-                System.out.printf("4 Imposto Pago no mês %.2f%n", mesPago);
-            }      
+            // else if(salario >= 3001.00 && salario <= 4500.00){
+            //     imposto = 0.18;
+            //     mesPago = salario * imposto;
+            //     System.out.printf("3 Imposto Pago por mês %.2f%n", mesPago);
+            // }
+            // else {
+            //     imposto = 0.28;
+            //     mesPago = salario * imposto;
+            //     System.out.printf("4 Imposto Pago no mês %.2f%n", mesPago);
+            // }      
     }
 }
